326d3c21dfc52c6cd9828774ae70c62e38b8a170
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-09-01 10:20:30 -0400

    Initial commit

---
+++ EnvironmentVariablesExample/src/main/java/com/appsdeveloperblog/aws/EnvironmentVariablesExample/EnvironmentVariablesLambda.java
@@ -0,0 +1,28 @@
+package com.appsdeveloperblog.aws.EnvironmentVariablesExample;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class EnvironmentVariablesLambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        return response;
+
+    }
+
+}
